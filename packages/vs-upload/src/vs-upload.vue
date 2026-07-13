44716f128c89f5d2469af0b6bd9f74d403543777
ashwinkshenoy <ashwinkshenoy@gmail.com>
2026-07-13 20:17:44 +0530

    chore: readme fix

--- packages/vs-upload/src/vs-upload.vue
+++ packages/vs-upload/src/vs-upload.vue
@@ -67,25 +67,7 @@
           :disabled="disabled"
           @click.stop="removeFile(index)"
         >
-          <!-- Trash icon -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            aria-hidden="true"
-          >
-            <polyline points="3 6 5 6 21 6" />
-            <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
-            <path d="M10 11v6" />
-            <path d="M14 11v6" />
-            <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" />
-          </svg>
+          <span class="vs-upload__remove-icon" aria-hidden="true" v-html="getRemoveIconSvg()"></span>
         </button>
       </li>
     </ul>
@@ -213,6 +195,10 @@
         updated.splice(index, 1);
         this.$emit('update:modelValue', updated);
         this.$emit('change', updated);
+      },
+
+      getRemoveIconSvg() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="3 6 5 6 21 6" /><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" /><path d="M10 11v6" /><path d="M14 11v6" /><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" /></svg>`;
       },
 
       getFileIconSvg(filename) {
@@ -460,6 +446,12 @@
         opacity: 0.4;
         cursor: not-allowed;
       }
+    }
+
+    &__remove-icon {
+      display: flex;
+      width: 15px;
+      height: 15px;
     }
 
     &__message {
